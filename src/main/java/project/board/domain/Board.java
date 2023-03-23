0ee47986f8b3a99e18965ce2db87fdb680ec4d69
tsi04188 <wnsgnlrkt@naver.com>
2023-03-23 23:53:22 +0900

    1

---
+++ src/main/java/project/board/domain/Board.java
@@ -0,0 +1,34 @@
+package project.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String writer;
+
+    private Integer view;
+
+    @CreationTimestamp
+    private LocalDateTime regDate;
+}
